6142e7aa18380a1e95822b92388fd0ec0c510844
Dave Knappenberger <kb7ky0@gmail.com>
2022-02-12 17:41:17 -0700

    update Dockerfiles to build all 3  debug variants of stdio, file, and none

--- createDocker.sh
+++ createDocker.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #
 # create docker images csp_bridge zmqproxy and libcspbuild
-docker build -t cspbase .
+docker build -t cspdev .
+docker build --no-cache -t csp-base-stdio -f Docketfile-csp-base-stdio
+docker build --no-cache -t csp-base-file -f Docketfile-csp-base-file
+docker build --no-cache -t csp-base-none -f Docketfile-csp-base-none
 docker build -t zmqproxy -f Dockerfile-zmqproxy .
 docker build -t cspbridge -f Dockerfile-cspbridge .
+docker build -t cspapps -f Dockerfile-test .
