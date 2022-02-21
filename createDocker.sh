75cb7dbfd8735948ad9b501a83a7dfebeb60d241
Dave Knappenberger <kb7ky0@gmail.com>
2022-02-21 02:59:47 +0000

    fix syntax errors in Dockerfile creation

--- createDocker.sh
+++ createDocker.sh
@@ -2,9 +2,9 @@
 #
 # create docker images csp_bridge zmqproxy and libcspbuild
 docker build -t cspdev .
-docker build --no-cache -t csp-base-stdio -f Docketfile-csp-base-stdio
-docker build --no-cache -t csp-base-file -f Docketfile-csp-base-file
-docker build --no-cache -t csp-base-none -f Docketfile-csp-base-none
+docker build --no-cache -t csp-base-stdio -f Dockerfile-csp-base-stdio .
+docker build --no-cache -t csp-base-file -f Dockerfile-csp-base-file .
+docker build --no-cache -t csp-base-none -f Dockerfile-csp-base-none .
 docker build -t zmqproxy -f Dockerfile-zmqproxy .
 docker build -t cspbridge -f Dockerfile-cspbridge .
 docker build -t cspapps -f Dockerfile-test .
