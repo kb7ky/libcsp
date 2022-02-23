e5a20e7bbec6b94f2e92b255572f4e5ec9adc516
Dave Knappenberger <kb7ky0@gmail.com>
2022-02-23 07:55:48 -0700

    Add functionality to Debug flag

--- examples/python_crc32_test.py
+++ examples/python_crc32_test.py
@@ -19,7 +19,7 @@ import libcsp_py3 as libcsp
 
 def getOptions():
     parser = argparse.ArgumentParser(description="Parses command.")
-    parser.add_argument("-D", "--debug", default=False, help="Turn on debug")
+    parser.add_argument("-D", "--debug", action='store_true', help="Turn on debug")
     return parser.parse_args(sys.argv[1:])
 
 
@@ -29,7 +29,8 @@ if __name__ == "__main__":
     
     buf = sys.stdin.buffer.read()
     
-    print("buf: " + buf.hex())
+    if options.debug == True:
+        print("buf: " + buf.hex())
+        print("Len: " + str(len(buf)))
 
-    print("Len: " + str(len(buf)))
     print("CRC: " + hex(libcsp.crc32_memory(buf, len(buf))))
